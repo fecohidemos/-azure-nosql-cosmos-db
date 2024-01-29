e6321a9c0326c909ed60514baf7e6e252a3c006f
fecohi <fernandocorteshierro@gmail.com>
2024-01-29 11:16:34 +0100

    fixed security issue

--- Python/config.py
+++ Python/config.py
@@ -1,8 +1,8 @@
 import os
 
 settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://catcosmosdbdemo.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'hqwRiYpnW0JwgtwyuLhe9a78cjrghZXc6QsPNzakapEvlGeDvgzFiluWnTLEQJgGWs9xUgyDwcxEACDb51SZaQ=='),
+    'host': os.environ['ACCOUNT_HOST'],
+    'master_key': os.environ['ACCOUNT_KEY'],
     'database_id': os.environ.get('COSMOS_DATABASE', 'ecommerce'),
     'container_id': os.environ.get('COSMOS_CONTAINER', 'orders'),
 }
